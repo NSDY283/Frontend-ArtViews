367e0f155461aff4d2997f52ce17c6e130c18097
DanielMauricioVilla <104464544+DanielMauricioVilla@users.noreply.github.com>
2023-06-22 04:45:10 -0500

    Update MyProfileE.jsx

--- src/components/pages/MyProfileE/MyProfileE.jsx
+++ src/components/pages/MyProfileE/MyProfileE.jsx
@@ -1,17 +1,79 @@
 import React from 'react'
 import { useState } from 'react'
+import axios from 'axios'
 import './MyProfileE.css'
 import { Link } from 'react-router-dom'
-import { ModalE } from '../../ui/ModalE/ModalE'
+import {  useNavigate } from 'react-router-dom'
 
 
 export const MyProfileE = () => {
 
     const [estadoModal1, cambiarEstadoModal1] = useState(false);
+    const [loading, setLoading]=useState(false);
     
 
-    const name =  JSON.parse(localStorage.getItem("name"));
+    const name1 =  JSON.parse(localStorage.getItem("name"));
     const gmail =  JSON.parse(localStorage.getItem("email"));
+    const idE =  JSON.parse(localStorage.getItem("idE"));
+
+    
+
+    const [inputs, setInputs] = useState({
+        // email: "",
+        name: "",
+        email: "",
+        password: "",
+    })
+
+    const [message, setMessage]=useState();
+    // const [loading, setLoading]=useState(false);
+    
+    
+    // const {email,nameP,img,category,price,cant,description}= inputs;
+    const {name, email, password}= inputs;
+    
+    const onChange = (e)=> {
+        setInputs({ ...inputs, [e.target.name]: e.target.value})
+    }
+    
+    const onSubmit = async (e, id) => {
+      e.preventDefault();
+    
+      const UserE = {
+          name, 
+          email,
+          password,
+      };
+    
+      console.log(UserE);
+      setLoading(true);
+    
+      try {
+          const response = await axios.put(`https://backend-artviews.onrender.com/api/users/clientE/${id}`, UserE);
+          setInputs({
+              nameP: "",
+              email: "",
+              password: "",
+          });
+    
+          setTimeout(() => {
+              setMessage("");
+              setLoading(false);
+              navigate('/LogIN')
+          }, 1500);
+      } catch (error) {
+          console.error(error);
+          setMessage("Hubo un error");
+    
+          setTimeout(() => {
+              setMessage("");
+              setLoading(false);
+          }, 1500);
+      }
+    };
+
+    const navigate = useNavigate();
+    
 
   return (
     <div className="profileE">
@@ -21,7 +83,7 @@ export const MyProfileE = () => {
         </div>
         <div className="ContainerProfileE2">
             <div className="containerE2-0">
-                <h1>{name}</h1>
+                <h1>{name1}</h1>
                 <p>{gmail}</p>
                 <p>Un emprendimiento basado en productos hechos a mano, apoyando el arte y la diversidad de este.</p>
 
@@ -34,8 +96,30 @@ export const MyProfileE = () => {
             </div>
         </div>
 
-        <ModalE estado = {estadoModal1} cambiarEstado = {cambiarEstadoModal1}>
-        </ModalE>
+        <div estado = {estadoModal1} cambiarEstado = {cambiarEstadoModal1}>
+        {estadoModal1 && 
+        <div className = "Overlay">
+            <div className="ContenedorModal">
+                <button className="BotonCerrar" onClick={() => cambiarEstadoModal1(false)}>
+                    X
+                </button>
+                <h1>Actualización de datos</h1>
+
+                <form action='' className="form2SingU" onSubmit={(e) => onSubmit(e, idE)}>
+
+                    <input onChange={(e) => onChange(e)} placeholder='Nombre de Usuario' name='name' type='text' id='nameUser2' required />
+                    <input onChange={(e) => onChange(e)} placeholder='Contraseña nueva' name='password' type='password' id='password2' required />
+                    <input onChange={(e) => onChange(e)} placeholder='Correo nuevo' name='email' type='email' id='emailSing2' required />
+
+                    
+                    
+                    <button className='btnAct2'onSubmit={() => alert('funciona')} ><p className="subPSing">{loading ? 'Cargando...' : 'Actualizar'}</p></button>
+                </form>
+            </div>
+        </div>
+        }
+        
+    </div>
     </div>
   )
 }
